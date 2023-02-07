a6a6297b8e66f4e5b92651e2c73b2d6ff8e07516
Gautam Kumarm <g2gautamsingh@gmail.com>
2023-02-07 15:30:33 +0530

    done

--- src/routes/user.js
+++ src/routes/user.js
@@ -12,13 +12,60 @@ app.get("/", async (req, res) => {
   }
 });
 
-app.post("/", async (req, res) => {
+app.post("/signup", async (req, res) => {
+  let { email } = req.body;
+  let user = await UserModel.findOne({ email });
   try {
-    let user = new User({ ...req.body });
-    await user.save();
-    res.status(200).send(user);
+    if (user) {
+      return res
+        .status(400)
+        .send(
+          "This email has already resigtered with another account please use different email"
+        );
+    }
+    let newUser = new User({ ...req.body });
+    await newUser.save();
+    return res.status(200).send(newUser);
   } catch (er) {
-    res.status(404).send(er.message);
+    return res.status(404).send(er.message);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(404).send({ msg: "These fields are required" });
+  }
+
+  try {
+    const user = await UserModel.findOne({ email, password });
+    if (user) {
+      return res
+        .status(200)
+        .send({ data: user, msg: "User login successfully" });
+    } else {
+      return res
+        .status(401)
+        .send({ msg: "Email or Password is not not match" });
+    }
+  } catch (er) {
+    return res.status(500).send({ msg: er.message });
+  }
+});
+
+app.get("/search", async (req, res) => {
+  let keyword = {};
+  if (req.query.q) {
+    keyword = req.query.q;
+  }
+  console.log(keyword);
+  try {
+    const AllUser = await User.find({
+      name: { $regex: keyword, $options: "i" },
+    });
+    return res.status(200).send(AllUser);
+  } catch (er) {
+    return res.status(404).send(er.message);
   }
 });
 
